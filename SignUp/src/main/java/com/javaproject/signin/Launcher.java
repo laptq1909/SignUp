3d778ae2635695bea1c8877ed7c39e28332befeb
laptq1909 <25021843@vnu.edu.vn>
2026-06-07 19:38:59 +0700

    Delete SignUp directory

--- SignUp/src/main/java/com/javaproject/signin/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.javaproject.signin;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(Run.class, args);
-    }
-}
